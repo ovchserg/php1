5cf9763f1462d38be6d820e21e555404be550d22
ovchserg <42462189+ovchserg@users.noreply.github.com>
2019-02-18 17:12:34 +0300

    6-ая домашняя работа

--- homework6/classes/Gallery.php
+++ homework6/classes/Gallery.php
@@ -6,7 +6,7 @@ class Gallery
 
     public function __construct()
     {
-        $this->images = array_diff( scandir(__DIR__ . '/../gallery/images/', SCANDIR_SORT_NONE ), ['.', '..'] );;
+        $this->images = array_diff( scandir(__DIR__ . '/../gallery/images/', SCANDIR_SORT_NONE ), ['.', '..'] );
     }
 
 }
