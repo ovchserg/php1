f5f5276a562407b98ef95b8df93c382d3631560f
ovchserg <42462189+ovchserg@users.noreply.github.com>
2019-03-14 18:15:15 +0300

    update 5

--- homework5/login.php
+++ homework5/login.php
@@ -16,8 +16,7 @@ if (
 if ( null !== getCurrentUser() ){
     header( 'Location: /homework5/index.php' );
     exit;
-}
-else { ?>
+} else { ?>
     <form  action="/homework5/login.php" method="post">
         <input type="text" name="login">
         <input type="password" name="password">
